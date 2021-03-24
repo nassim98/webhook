372ba4846a1f7095f6ce43ecac8c04798a9dfebf
ENOUALI Nassim <nenouali@alteca.fr>
2021-03-24 11:42:55 +0100

    port config

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.json({'message': 'Server is running'});
-});
+}).listen(PORT, () => console.log(`Listening on ${ PORT }`));
